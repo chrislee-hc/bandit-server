3eebcdebc91184ecc2bbccc1f31c35652f541a4e
Christopher Lee <christopherlee@college.harvard.edu>
2022-11-29 20:26:43 -0500

    added compression

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-var cors = require("cors");
+const cors = require("cors");
 const helmet = require("helmet");
+const compression = require("compression");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
 
-var app = express();
+const app = express();
 app.use(cors());
 
 app.use(helmet());
+app.use(compression());
 
 app.use(logger("dev"));
 app.use(express.json());
